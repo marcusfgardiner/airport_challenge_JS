728636bbcae416a00c2d0ceb51836b08b08a22ca
marcusfgardiner <marcusfgardiner@gmail.com>
2018-02-20 17:45:44 +0000

    Have land and take off methods working correctly, including stubbing for weather. Next steps: use dependency injection for airport when defining the object. Change stubbing as currently hacking around the fact can only spy once

--- spec/AirportSpec.js
+++ spec/AirportSpec.js
@@ -8,22 +8,28 @@ describe('Airport', function() {
     plane = {};
   });
   describe('.land', function() {
-    it('can land a plane', function() {
-      airport.land(plane);
+    it('can land a plane into the hangar', function() {
+      spyOn(weather, 'isStormy').and.returnValue(false);
+      airport.land(plane, weather);
       expect(airport.hangar).toContain(plane);
+    });
+    it("won't land a plane when stormy", function() {
+      spyOn(weather, 'isStormy').and.returnValue(true);
+      expect( function() {airport.land(plane, weather);}).toThrow('The weather is crazy');
     });
   });
   describe('.takeOff', function() {
-    it('can take off a plane', function(){
+    it('can take off a plane removes it from the hangar', function() {
       // spyOn(Math, 'random').and.returnValue(0.1);
       spyOn(weather, 'isStormy').and.returnValue(false);
-      airport.land(plane);
+      airport.land(plane, weather);
       airport.takeOff(plane, weather);
       expect(airport.hangar).not.toContain(plane);
     });
     it("won't take off when stormy", function() {
-      console.log(airport);
-      airport.land(plane);
+      // TODO: hacking the spyOn so that it can be used twice...
+      spyOn(Math, 'random').and.returnValue(0.1);
+      airport.land(plane, weather);
       // spyOn(Math, 'random').and.returnValue(0.8);
       spyOn(weather, 'isStormy').and.returnValue(true);
       expect( function(){ airport.takeOff(plane, weather); } ).toThrow('The weather is crazy');
--- src/Airport.js
+++ src/Airport.js
@@ -1,9 +1,15 @@
+// TODO: Put dependency injection in at point of defintion
+
 var Airport = function() {
   this.hangar = [];
 };
 
-Airport.prototype.land = function (plane) {
-  return this.hangar.push(plane);
+Airport.prototype.land = function (plane, weather) {
+  if (weather.isStormy()) {
+    throw 'The weather is crazy';
+  } else {
+    return this.hangar.push(plane);
+  }
 };
 
 Airport.prototype.takeOff = function (plane, weather) {
